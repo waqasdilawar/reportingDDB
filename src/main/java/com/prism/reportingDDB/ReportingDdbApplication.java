dba5a2033afef13ed84c9930fc1e15507ae24bae
waqasdilawar <waqas.dilawar@discorp.com>
2020-10-15 18:10:04 +0500

    initial commit

---
+++ src/main/java/com/prism/reportingDDB/ReportingDdbApplication.java
@@ -0,0 +1,13 @@
+package com.prism.reportingDDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingDdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingDdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prism/reportingDDB/ReportingDdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prism.reportingDDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingDdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
